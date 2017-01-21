4c621b44042795c28471603b4aecbeb342596939
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2017-01-21 21:28:43 +0200

    Added main view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'app works!';
+  credentials = {
+    email: "",
+    password: ""
+  };
+
+  item: FirebaseObjectObservable<any>;
+
+  constructor(public af: AngularFire) {
+    this.item = af.database.object('/item');
+
+    this.af.auth.subscribe(auth => console.log(auth))
+  }
+
+  public saveItem(){
+    const itemObservable = this.af.database.object('/item');
+    itemObservable.set({ name: 'ohter mother name!'});
+  }
+
+  public signUp(email: string, password: string){
+    this.af.auth.createUser({email: email, password: password})
+  }
+
+  public login(email: string, password: string){
+    console.log(email, password);
+    this.af.auth.login({email: email, password: password});
+  }
+
+  public logout() {
+    this.af.auth.logout();
+ }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+
+const myFirebaseConfig = {
+  apiKey: "AIzaSyDeSLDhiKX1cXFelc1Zz1F76lTxOy8K8FQ",
+  authDomain: "matcher-40e60.firebaseapp.com",
+  databaseURL: "https://matcher-40e60.firebaseio.com",
+  storageBucket: "matcher-40e60.appspot.com",
+  messagingSenderId: "800915537902"
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(myFirebaseConfig, myFirebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
